3c2d38d99a4ad4f5bfe27af853721eea75ca1790
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-08-20 19:41:41 -0700

    add fields to create and response DTO

--- src/main/java/com/sasha/carshop/dto/CreateCarDTO.java
+++ src/main/java/com/sasha/carshop/dto/CreateCarDTO.java
@@ -1,4 +1,22 @@
 package com.sasha.carshop.dto;
 
+import com.sasha.carshop.enums.DriveType;
+import com.sasha.carshop.enums.TransmissionType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CreateCarDTO {
+
+    private String model;
+    private Integer yearOfIssue;
+    private Integer mileage;
+    private String bodyType;
+    private String color;
+    private Double engineSize;
+    private Integer horsePower;
+    private TransmissionType transmissionType;
+    private DriveType driveType;
+    private String vin;
 }
--- src/main/java/com/sasha/carshop/dto/ResponseCarDTO.java
+++ src/main/java/com/sasha/carshop/dto/ResponseCarDTO.java
@@ -1,4 +1,23 @@
 package com.sasha.carshop.dto;
 
+import com.sasha.carshop.enums.DriveType;
+import com.sasha.carshop.enums.TransmissionType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ResponseCarDTO {
+
+    private Long id;
+    private String model;
+    private Integer yearOfIssue;
+    private Integer mileage;
+    private String bodyType;
+    private String color;
+    private Double engineSize;
+    private Integer horsePower;
+    private TransmissionType transmissionType;
+    private DriveType driveType;
+    private String vin;
 }
