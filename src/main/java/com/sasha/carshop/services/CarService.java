d60943d6e903c77a44c4c58cc89fad195232859d
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-08-20 20:08:38 -0700

    add create car method to service class

--- src/main/java/com/sasha/carshop/services/CarService.java
+++ src/main/java/com/sasha/carshop/services/CarService.java
@@ -1,4 +1,23 @@
 package com.sasha.carshop.services;
 
+import com.sasha.carshop.dto.CreateCarDTO;
+import com.sasha.carshop.dto.ResponseCarDTO;
+import com.sasha.carshop.entity.Car;
+import com.sasha.carshop.mapper.CarMapper;
+import com.sasha.carshop.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CarService {
+
+    private final CarRepository carRepository;
+
+    @Transactional
+    public ResponseCarDTO createCar(CreateCarDTO carDTO) {
+
+       return CarMapper.fromEntity(carRepository.save(CarMapper.toEntity(carDTO)));
+    }
 }
