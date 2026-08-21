b75e2c93cc4e32744a0596b55319e6bec28fb33a
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-08-21 11:01:05 -0700

    add updateCar method to CarMapper class

--- src/main/java/com/sasha/carshop/mapper/CarMapper.java
+++ src/main/java/com/sasha/carshop/mapper/CarMapper.java
@@ -2,6 +2,7 @@ package com.sasha.carshop.mapper;
 
 import com.sasha.carshop.dto.CreateCarDTO;
 import com.sasha.carshop.dto.ResponseCarDTO;
+import com.sasha.carshop.dto.UpdateCarDTO;
 import com.sasha.carshop.entity.Car;
 
 public class CarMapper {
@@ -38,5 +39,19 @@ public class CarMapper {
         dto.setVin(car.getVin());
 
         return dto;
+    }
+
+    public static void updateCar(Car car, UpdateCarDTO dto) {
+
+        car.setModel(dto.getModel());
+        car.setYearOfIssue(dto.getYearOfIssue());
+        car.setMileage(dto.getMileage());
+        car.setBodyType(dto.getBodyType());
+        car.setColor(dto.getColor());
+        car.setEngineSize(dto.getEngineSize());
+        car.setHorsePower(dto.getHorsePower());
+        car.setTransmissionType(dto.getTransmissionType());
+        car.setDriveType(dto.getDriveType());
+        car.setVin(dto.getVin());
     }
 }
