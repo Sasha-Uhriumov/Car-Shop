0759a3a7a087dd96c505d5f75a9244e7424498ad
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-08-21 18:35:59 -0700

    add unit test for createCar

--- src/test/java/com/sasha/carshop/services/CarServiceTest.java
+++ src/test/java/com/sasha/carshop/services/CarServiceTest.java
@@ -1,5 +1,6 @@
 package com.sasha.carshop.services;
 
+import com.sasha.carshop.dto.CreateCarDTO;
 import com.sasha.carshop.dto.ResponseCarDTO;
 import com.sasha.carshop.dto.UpdateCarDTO;
 import com.sasha.carshop.entity.Car;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -99,6 +101,57 @@ class CarServiceTest {
         when(carRepository.findById(carId)).thenReturn(Optional.of(existsCar));
 
         ResponseCarDTO result = carService.getCarById(carId);
+
+        assertNotNull(result);
+        assertEquals(carId, result.getId());
+        assertEquals("BMW", result.getModel());
+        assertEquals(2000, result.getYearOfIssue());
+        assertEquals(20000, result.getMileage());
+        assertEquals("sedan", result.getBodyType());
+        assertEquals("red", result.getColor());
+        assertEquals(2.5, result.getEngineSize());
+        assertEquals(905, result.getHorsePower());
+        assertEquals(TransmissionType.AUTOMATIC, result.getTransmissionType());
+        assertEquals(DriveType.AWD, result.getDriveType());
+        assertEquals("j3jre934r9msdm3ew", result.getVin());
+    }
+
+    @Test
+    void createCar() {
+
+        CreateCarDTO carDTO = new CreateCarDTO();
+
+        carDTO.setModel("BMW");
+        carDTO.setYearOfIssue(2000);
+        carDTO.setMileage(20000);
+        carDTO.setBodyType("sedan");
+        carDTO.setColor("red");
+        carDTO.setEngineSize(2.5);
+        carDTO.setHorsePower(905);
+        carDTO.setTransmissionType(TransmissionType.AUTOMATIC);
+        carDTO.setDriveType(DriveType.AWD);
+        carDTO.setVin("j3jre934r9msdm3ew");
+
+        Long carId = 1L;
+
+        Car carSaved = new Car();
+
+        carSaved.setId(carId);
+        carSaved.setModel("BMW");
+        carSaved.setYearOfIssue(2000);
+        carSaved.setMileage(20000);
+        carSaved.setBodyType("sedan");
+        carSaved.setColor("red");
+        carSaved.setEngineSize(2.5);
+        carSaved.setHorsePower(905);
+        carSaved.setTransmissionType(TransmissionType.AUTOMATIC);
+        carSaved.setDriveType(DriveType.AWD);
+        carSaved.setVin("j3jre934r9msdm3ew");
+
+        when(carRepository.existsCarByVin("j3jre934r9msdm3ew")).thenReturn(false);
+        when(carRepository.save(any(Car.class))).thenReturn(carSaved);
+
+        ResponseCarDTO result = carService.createCar(carDTO);
 
         assertNotNull(result);
         assertEquals(carId, result.getId());
